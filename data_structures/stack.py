e9b55c43f632e135e4d75960bb32203e8f16fd23
btcross26 <btcross26@yahoo.com>
2016-08-25 10:07:47 -0400

    added documentation for stack module

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -14,9 +14,22 @@ class FullError(Exception):
     pass
     
 class Stack(object):
+    '''
+    Class Stack implements stack functionality using an underlying instance
+    of a DoubleLinkedList.
+    '''
     def __init__(self, maxsize = None):
+        '''
+        Constructor Arguments
+        ---------------------
+        maxsize: int
+            Maximum capacity of the stack instance.
+        '''
         self._maxsize = maxsize
         self._stack = DoubleLinkedList()
+        
+    def __str__(self):
+        return list(self).__str__()
         
     def __repr__(self):
         return "Stack(maxsize={:s})".format(str(self._maxsize))
@@ -28,15 +41,36 @@ class Stack(object):
     def __len__(self):
         return len(self._stack)
         
+    def max_capacity(self):
+        '''
+        Returns the max capacity of the Stack instance if specified. If the
+        max capacity if not limited, returns -1.
+        '''
+        if self._maxsize is not None:
+            return self._maxsize
+        else:
+            return -1
+        
     def push(self, value):
+        '''
+        Pushes value onto the Stack instance. If the maximum capacity of the
+        stack is limited, raises a FullError.
+        '''
         if self._maxsize is not None and len(self) == self._maxsize:
             raise FullError("<method>:push called on full Stack")
         self._stack.append(value)
         
-    def pop(self, value):
+    def pop(self):
+        '''
+        Pops and returns the next Stack instance value. If the Stack instance
+        is empty, raised an EmptyError.
+        '''
         if self.empty():
             raise EmptyError("<method>:pop called on empty Stack")
         return self._stack.popback()
         
-    def empty(self, value):
+    def empty(self):
+        '''
+        Returns True if the Stack instance is empty, False otherwise.
+        '''
         return len(self) == 0
